c16e2bab69c0540aab2cefdc3d3e4fcc7bbcb142
pnumin <pnumin@pusan.ac.kr>
2023-01-20 07:56:02 +0900

    배열

---
+++ 04/scripts/index.js
@@ -0,0 +1,19 @@
+
+//폭탄이 있는 위치를 나타내는 배열
+//DOM이 로드가 되면 반복문을 이용하여 [0,0,0,0,0,0,0,0,1]로 초기화
+let num = [0,0,0,0,0,0,0,0,1];
+
+//박스를 선택한 순서를 기록하는 배열
+let selNum = [];
+
+//박스를 선택한 개수를 기록하는 변수
+let cnt = 0 ;
+
+//폭탄이 섞였는지 체크하는 flag변수
+let shuffleFlag = false;
+
+
+/* DOM이 로드된 후에 클릭이벤트 연결*/
+document.addEventListener("DOMContentLoaded", ()=>{
+   
+});
